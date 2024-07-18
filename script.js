a273bc0d5434a8819a7641c0b130e208d0b3ab28
Ravisharma-1999 <ravi829144@gmail.com>
2024-07-18 17:29:17 +0530

    created a submission

--- script.js
+++
@@ -1,9 +0,0 @@
-cy.visit("http://localhost:3000/");
-cy.wait(2000); // Adjust wait time as needed
-cy.get(".link-boxes").should("have.css", "display", "flex");
-cy.get(".link-boxes").contains(8879887262);
-cy.get(".link-boxes").contains("Home");
-cy.get(".link-boxes").contains("About");
-cy.get(".link-boxes").contains("Links");
-cy.get(".link-boxes").contains("Services");
-cy.get(".link-boxes").contains("Contact");
